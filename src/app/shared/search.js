3c0d8c8e7d8a87cded548b0035d2a678bb780801
mohamedsaiedd <mohamed.saied.fathallah@gmail.com>
2022-03-30 11:52:28 +0200

    handle search bar

--- src/app/shared/search.js
+++ src/app/shared/search.js
@@ -22,44 +22,52 @@ const Search = () => {
     }, []);
 
     const [Products, SetProducts] = useState([])
-    const [ SearchTarget, setSearchTarget] = useState("");
+    const [SearchTarget, setSearchTarget] = useState("");
 
-
+    let resultsChanged =  false    ;
 
     return (
-        <div>
+        <div className="wrapperSearch">
             <div className="wrapper">
                 <div className="search">
                     <input
-
                         placeholder="Search a color by name..."
                         onChange={
                             (e) => {
                                 setSearchTarget(e.target.value);
-                                console.log("value",e.target.value)
+                                console.log("value", e.target.value)
+                                resultsChanged = true;
+                                console.log("changed")
                             }
                         }
-
                         autocomplete="on"
                     />
                     <i class="fas fa-search"></i>
                 </div>
 
             </div>
-            {Products.filter((product) => {
-                if (SearchTarget == "") {
-                    return product
-                } else if ( product.title.toLowerCase().includes(SearchTarget)) {
-                    return product
-                }
+               {
+               
+               
+                (resultsChanged == true) ?
 
-            }).map((product , key ) => {
-                return(
-                    <div key={key}>
-                        <p>{product.title}</p>
-                    </div>
-                )
-            })
+                Products.filter((product) => {
+                    if (SearchTarget === "") {
+                        return product
+                    } else if (product.title.toLowerCase().includes(SearchTarget)) {
+                        return product
+                    }
+                }).map((product, key) => {
+                    return (
+                        <div key={key} className="searchResult" >
+                            <p> <span>{product.category}</span>
+                                {product.title} </p>
+                        </div>
+                    )
+                }) :
+                resultsChanged == false
+                
+                
             }
         </div>
     )

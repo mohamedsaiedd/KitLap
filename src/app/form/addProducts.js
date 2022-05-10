d28f61d02a54b89fd7662893e4ba7572c174482a
yossry98 <m98yossry@gmail.com>
2022-05-10 13:00:22 +0200

    handling add product method

--- src/app/form/addProducts.js
+++ src/app/form/addProducts.js
@@ -1,38 +1,54 @@
-// import React , {useEffect , useState} from 'react';
-// import axios from "axios"
+import React , {useState , useRef  }  from 'react';
+import axios from "axios"
 
-// const AddProducts = () => {
-//     // const [AddProduct, setAddProduct] = useState({title : ""})
+const AddProducts = () => {
+    const [AddProduct, setAddProduct] = useState({})
 
+//using ref for selecting dom elements
+        const titleRef = useRef(null);
+        const imageRef = useRef(null);
+        const descriptonRef = useRef(null);
+        const priceRef = useRef(null);
+        const discountRateRef = useRef(null);
+
+        const url = "https://kitlap.monady.tk/api/Products/AddProduct"
+        const pushProduct = () => {
+
+            titleRef.current = AddProduct.title
+            console.log(titleRef.current)
+            imageRef.current = AddProduct.imageUrl
+            descriptonRef.current = AddProduct.description
+            priceRef.current = AddProduct.price
+            discountRateRef.current = AddProduct.discountRate
+        
+            axios.post(url,  AddProduct ).then(res => {
+                console.log(res);
+                console.log(res.data);
+            })
+        }
+
+        return (
+            //  <button onClick={pushProduct}>AddProduct</button>
+         
+                <div>
+                    {/* product Title: */}
+                    <input ref={titleRef} type="text" name="title"  /> <br/>
+                    product Image:
+                    <input ref={imageRef} type="file" name="image"  /> <br/>
+                    product descripton:
+                    <input ref={descriptonRef} type="text" name="description"  /><br/>
+                    product price:
+                    <input  ref={priceRef} type="text" name="price"/><br/>
+                    product discountRate:
+                    <input ref={discountRateRef} type="text" name="discountRate" /><br/> 
+            
+                      <button onClick={pushProduct}>AddProduct</button>
+                </div>
+           
+         
+        )
     
-       
-//         const product = {
-//             "title" :"mohamed",
-//             "description": "string",
-//             "imageUrl": "string",
-//             "price": 0,
-//             "hasDiscount": true,
-//             "discountRate": 0
-//         }
-//         const url = "https://kitlap.monady.tk/api/Products/AddProduct"
-//         const push = () => {
-//             axios.post(url,  product ).then(res => {
-//                 console.log(res);
-//                 console.log(res.data);
-//             })
-//         }
 
-//         return (
-//              <button onClick={push}>AddProduct</button>
-//             // <form onSubmit={push}>
-//             //     <label>
-//             //         product Title:
-//             //         <input type="text" name="title" onChange={(e)=> setAddProduct(e.tasrget.value)} />
-//             //     </label>
-//             //     <button type="submit">Add</button>
-//             // </form>
-//         )
-    
+}
 
-// }
-// export default AddProducts
+export default AddProducts
